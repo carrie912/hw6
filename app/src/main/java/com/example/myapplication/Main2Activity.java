f098970da80e99f14bb33d39e6d2ed568dec5b0e
carrie91233 <57000421+carrie91233@users.noreply.github.com>
2019-11-01 16:03:39 +0800

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/Main2Activity.java
@@ -0,0 +1,27 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        final Button button1 = (Button) findViewById(R.id.Apagebutton);
+        button1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(Main2Activity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+
+}
